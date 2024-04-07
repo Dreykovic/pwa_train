fbb27da279f75cf7adb0a20c7a750ba0f36f4106
audrey <amonaaudrey16@gmail.com>
2024-04-07 20:43:21 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// create constants for the form and the form controls
+const newPeriodFormEl = document.getElementsByTagName("form")[0];
+const startDateInputEl = document.getElementById("start-date");
+const endDateInputEl = document.getElementById("end-date");
+
+// Listen to form submissions.
+newPeriodFormEl.addEventListener("submit", (event) => {
+  // Prevent the form from submitting to the server
+  // since everything is client-side.
+  event.preventDefault();
+
+  // Get the start and end dates from the form.
+  const startDate = startDateInputEl.value;
+  const endDate = endDateInputEl.value;
+
+  // Check if the dates are invalid
+  if (checkDatesInvalid(startDate, endDate)) {
+    // If the dates are invalid, exit.
+    return;
+  }
+
+  // Store the new period in our client-side storage.
+  storeNewPeriod(startDate, endDate);
+
+  // Refresh the UI.
+  renderPastPeriods();
+
+  // Reset the form.
+  newPeriodFormEl.reset();
+});
