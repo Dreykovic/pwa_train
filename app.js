41c9b7e802dbb0b80b3328f67c8926d759eaf821
audrey <amonaaudrey16@gmail.com>
2024-04-07 20:49:56 +0000

    Retrieve, append, sort, and re-store data

--- app.js
+++ app.js
@@ -28,3 +28,50 @@ newPeriodFormEl.addEventListener("submit", (event) => {
   // Reset the form.
   newPeriodFormEl.reset();
 });
+
+function checkDatesInvalid(startDate, endDate) {
+  // Check that end date is after start date and neither is null.
+  if (!startDate || !endDate || startDate > endDate) {
+    // To make the validation robust we could:
+    // 1. add error messaging based on error type
+    // 2. Alert assistive technology users about the error
+    // 3. move focus to the error location
+    // instead, for now, we clear the dates if either
+    // or both are invalid
+    newPeriodFormEl.reset();
+    // as dates are invalid, we return true
+    return true;
+  }
+  // else
+  return false;
+}
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "period-tracker";
+
+function storeNewPeriod(startDate, endDate) {
+  // Get data from storage.
+  const periods = getAllStoredPeriods();
+
+  // Add the new period objet to the end of the array of period objects.
+  periods.push({ startDate, endDate });
+
+  // Sort the array so that periods are ordered by start date, from newest
+  // to oldest.
+  periods.sort((a, b) => {
+    return new Date(b.startDate) - new Date(a.startDate);
+  });
+
+  // Store the updated array back in the storage.
+  window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
+}
+
+function getAllStoredPeriods() {
+  // Get the string of period data from localStorage
+  const data = window.localStorage.getItem(STORAGE_KEY);
+
+  // If no periods were stored, default to an empty array
+  // otherwise, return the stored data as parsed JSON
+  const periods = data ? JSON.parse(data) : [];
+
+  return periods;
+}
