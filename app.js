100aa618d1f265458b55326d8efc87b9cb219d2d
audrey <amonaaudrey16@gmail.com>
2024-04-07 20:52:26 +0000

    js functionnalities

--- app.js
+++ app.js
@@ -1,77 +1,73 @@
-// create constants for the form and the form controls
 const newPeriodFormEl = document.getElementsByTagName("form")[0];
 const startDateInputEl = document.getElementById("start-date");
 const endDateInputEl = document.getElementById("end-date");
+const pastPeriodContainer = document.getElementById("past-periods");
+
+// Add the storage key as an app-wide constant
+const STORAGE_KEY = "period-tracker";
 
 // Listen to form submissions.
 newPeriodFormEl.addEventListener("submit", (event) => {
-  // Prevent the form from submitting to the server
-  // since everything is client-side.
   event.preventDefault();
-
-  // Get the start and end dates from the form.
   const startDate = startDateInputEl.value;
   const endDate = endDateInputEl.value;
-
-  // Check if the dates are invalid
   if (checkDatesInvalid(startDate, endDate)) {
-    // If the dates are invalid, exit.
     return;
   }
-
-  // Store the new period in our client-side storage.
   storeNewPeriod(startDate, endDate);
-
-  // Refresh the UI.
   renderPastPeriods();
-
-  // Reset the form.
   newPeriodFormEl.reset();
 });
 
 function checkDatesInvalid(startDate, endDate) {
-  // Check that end date is after start date and neither is null.
   if (!startDate || !endDate || startDate > endDate) {
-    // To make the validation robust we could:
-    // 1. add error messaging based on error type
-    // 2. Alert assistive technology users about the error
-    // 3. move focus to the error location
-    // instead, for now, we clear the dates if either
-    // or both are invalid
     newPeriodFormEl.reset();
-    // as dates are invalid, we return true
     return true;
   }
-  // else
   return false;
 }
-// Add the storage key as an app-wide constant
-const STORAGE_KEY = "period-tracker";
 
 function storeNewPeriod(startDate, endDate) {
-  // Get data from storage.
   const periods = getAllStoredPeriods();
-
-  // Add the new period objet to the end of the array of period objects.
   periods.push({ startDate, endDate });
-
-  // Sort the array so that periods are ordered by start date, from newest
-  // to oldest.
   periods.sort((a, b) => {
     return new Date(b.startDate) - new Date(a.startDate);
   });
-
-  // Store the updated array back in the storage.
   window.localStorage.setItem(STORAGE_KEY, JSON.stringify(periods));
 }
 
 function getAllStoredPeriods() {
-  // Get the string of period data from localStorage
   const data = window.localStorage.getItem(STORAGE_KEY);
-
-  // If no periods were stored, default to an empty array
-  // otherwise, return the stored data as parsed JSON
   const periods = data ? JSON.parse(data) : [];
-
+  console.dir(periods);
+  console.log(periods);
   return periods;
 }
+
+function renderPastPeriods() {
+  const pastPeriodHeader = document.createElement("h2");
+  const pastPeriodList = document.createElement("ul");
+  const periods = getAllStoredPeriods();
+  if (periods.length === 0) {
+    return;
+  }
+  pastPeriodContainer.innerHTML = "";
+  pastPeriodHeader.textContent = "Past periods";
+  periods.forEach((period) => {
+    const periodEl = document.createElement("li");
+    periodEl.textContent = `From ${formatDate(
+      period.startDate,
+    )} to ${formatDate(period.endDate)}`;
+    pastPeriodList.appendChild(periodEl);
+  });
+
+  pastPeriodContainer.appendChild(pastPeriodHeader);
+  pastPeriodContainer.appendChild(pastPeriodList);
+}
+
+function formatDate(dateString) {
+  const date = new Date(dateString);
+  return date.toLocaleDateString("fr-FR", { timeZone: "UTC" });
+}
+
+renderPastPeriods();
