4cd550e4199190eeaca5fb6508303a5da0ca6f51
audrey <amonaaudrey16@gmail.com>
2024-04-08 11:57:04 +0000

    add pirate service worker train

---
+++ pirate/service-worker.js
@@ -0,0 +1,45 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('version1').then((cache) => {
+      return cache.addAll(
+        [
+          'index.html',
+          '/pirates.html',
+          '/styles/pirates.css',
+          '/styles/pirate.ttf',
+          '/images/i-love-pirates.jpg',
+          'offline.html'
+        ]);
+    })
+  );
+});
+
+
+self.addEventListener('fetch', (event) => {
+  if (!navigator.onLine && event.request.url.indexOf('index.html') !== -1) {
+    event.respondWith(showOfflineLanding(event));
+  }
+  else {
+    event.respondWith(pullFromCache(event));
+  }
+
+});
+
+function showOfflineLanding(event) {
+  return caches.match(new Request('offline.html'));
+}
+
+function pullFromCache(event) {
+  if (!event.request.url.startsWith('http')) {
+    //skip request
+    return;
+  }
+  return caches.match(event.request).then((response) => {
+    return response || fetch(event.request).then((response) => {
+      return caches.open('version1').then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    });
+  });
+}
